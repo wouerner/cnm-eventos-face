88ba2ad8960a0feb501db3fdbad67d616c3c0aaa
wouerner <wouerner@protonmail.com>
2018-10-24 16:38:26 -0300

    Logo

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,9 @@
 <template>
   <body id="app">
-      <nav class="navbar is-fixed-top" role="navigation" aria-label="main navigation">
+      <nav class="navbar is-fixed-top has-background-light" role="navigation" aria-label="main navigation">
           <div class="navbar-brand">
               <a class="navbar-item" href="https://bulma.io">
-                  <img src="https://bulma.io/images/bulma-logo.png" width="112" height="28">
+                  <img src="./assets/logo.png" width="112" height="28">
               </a>
               <a role="button" class="navbar-burger burger" aria-label="menu" aria-expanded="false" data-target="navbarBasicExample">
                   <span aria-hidden="true"></span>
